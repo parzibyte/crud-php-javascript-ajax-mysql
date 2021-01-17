930501a7eab9cd5cff894068f259151d09d4acf0
Luis Cabrera Benito <parzibyte@gmail.com>
2021-01-17 13:14:27 -0600

    Update guardar_producto.php

--- guardar_producto.php
+++ guardar_producto.php
@@ -30,6 +30,7 @@ También tengo canal de YouTube: https://www.youtube.com/channel/UCroP4BTWjfM0Ck
 $cargaUtil = json_decode(file_get_contents("php://input"));
 // Si no hay datos, salir inmediatamente indicando un error 500
 if (!$cargaUtil) {
+    // https://parzibyte.me/blog/2021/01/17/php-enviar-codigo-error-500/
     http_response_code(500);
     exit;
 }
