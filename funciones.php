3db566735baf20e6338a674471cfbed61b801f91
Luis Cabrera Benito <parzibyte@gmail.com>
2021-01-17 10:30:08 -0600

    Limpieza de código

--- funciones.php
+++ funciones.php
@@ -28,69 +28,12 @@ También tengo canal de YouTube: https://www.youtube.com/channel/UCroP4BTWjfM0Ck
 */ ?>
 <?php
 
-function obtenerProductosEnCarrito()
-{
-    $bd = obtenerConexion();
-    iniciarSesionSiNoEstaIniciada();
-    $sentencia = $bd->prepare("SELECT productos.id, productos.nombre, productos.descripcion, productos.precio
-     FROM productos
-     INNER JOIN carrito_usuarios
-     ON productos.id = carrito_usuarios.id_producto
-     WHERE carrito_usuarios.id_sesion = ?");
-    $idSesion = session_id();
-    $sentencia->execute([$idSesion]);
-    return $sentencia->fetchAll();
-}
-function quitarProductoDelCarrito($idProducto)
-{
-    $bd = obtenerConexion();
-    iniciarSesionSiNoEstaIniciada();
-    $idSesion = session_id();
-    $sentencia = $bd->prepare("DELETE FROM carrito_usuarios WHERE id_sesion = ? AND id_producto = ?");
-    return $sentencia->execute([$idSesion, $idProducto]);
-}
 
 function obtenerProductos()
 {
     $bd = obtenerConexion();
     $sentencia = $bd->query("SELECT id, nombre, descripcion, precio FROM productos");
     return $sentencia->fetchAll();
-}
-function productoYaEstaEnCarrito($idProducto)
-{
-    $ids = obtenerIdsDeProductosEnCarrito();
-    foreach ($ids as $id) {
-        if ($id == $idProducto) return true;
-    }
-    return false;
-}
-
-function obtenerIdsDeProductosEnCarrito()
-{
-    $bd = obtenerConexion();
-    iniciarSesionSiNoEstaIniciada();
-    $sentencia = $bd->prepare("SELECT id_producto FROM carrito_usuarios WHERE id_sesion = ?");
-    $idSesion = session_id();
-    $sentencia->execute([$idSesion]);
-    return $sentencia->fetchAll(PDO::FETCH_COLUMN);
-}
-
-function agregarProductoAlCarrito($idProducto)
-{
-    // Ligar el id del producto con el usuario a través de la sesión
-    $bd = obtenerConexion();
-    iniciarSesionSiNoEstaIniciada();
-    $idSesion = session_id();
-    $sentencia = $bd->prepare("INSERT INTO carrito_usuarios(id_sesion, id_producto) VALUES (?, ?)");
-    return $sentencia->execute([$idSesion, $idProducto]);
-}
-
-
-function iniciarSesionSiNoEstaIniciada()
-{
-    if (session_status() !== PHP_SESSION_ACTIVE) {
-        session_start();
-    }
 }
 
 function eliminarProducto($id)
