ba0dc14e19e3893816c2403f7e750ab6f590203c
Luis Cabrera Benito <parzibyte@gmail.com>
2021-01-17 10:59:16 -0600

    Limpieza y créditos

--- js/productos.js
+++ js/productos.js
@@ -1,3 +1,30 @@
+/*
+
+  ____          _____               _ _           _       
+ |  _ \        |  __ \             (_) |         | |      
+ | |_) |_   _  | |__) |_ _ _ __ _____| |__  _   _| |_ ___ 
+ |  _ <| | | | |  ___/ _` | '__|_  / | '_ \| | | | __/ _ \
+ | |_) | |_| | | |  | (_| | |   / /| | |_) | |_| | ||  __/
+ |____/ \__, | |_|   \__,_|_|  /___|_|_.__/ \__, |\__\___|
+         __/ |                               __/ |        
+        |___/                               |___/         
+    
+____________________________________
+/ Si necesitas ayuda, contáctame en \
+\ https://parzibyte.me               /
+ ------------------------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+Creado por Parzibyte (https://parzibyte.me).
+------------------------------------------------------------------------------------------------
+Si el código es útil para ti, puedes agradecerme siguiéndome: https://parzibyte.me/blog/sigueme/
+Y compartiendo mi blog con tus amigos
+También tengo canal de YouTube: https://www.youtube.com/channel/UCroP4BTWjfM0CkGB6AFUoBg?sub_confirmation=1
+------------------------------------------------------------------------------------------------
+*/
 const $cuerpoTabla = document.querySelector("#cuerpoTabla");
 
 const obtenerProductos = async () => {
@@ -74,7 +101,6 @@ const obtenerProductos = async () => {
         const $celdaBoton = document.createElement("td");
         $celdaBoton.appendChild($botonEliminar);
         $fila.appendChild($celdaBoton);
-        // Y ahora el botón 
         // Adjuntamos la fila a la tabla
         $cuerpoTabla.appendChild($fila);
     }
